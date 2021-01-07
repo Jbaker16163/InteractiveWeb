02aaea7bef144c2d11086ef17f12e9767be57fd0
Ghios <56654758+Jbaker16163@users.noreply.github.com>
2021-01-06 22:51:14 -0500

    Add Azure deployment files

--- interactiveProject/settings.py
+++ interactiveProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%$gn2#!60ook^gces63+5_frjmw%(bz26rhbkwj7l9^c03^$cn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘*’]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
