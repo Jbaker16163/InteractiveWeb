e1e8a7ee2b86806a4f6bb58b63d609baf555ff51
Ghios <56654758+Jbaker16163@users.noreply.github.com>
2021-01-07 15:50:21 -0500

    Website Fix 2

--- interactiveProject/settings.py
+++ interactiveProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%$gn2#!60ook^gces63+5_frjmw%(bz26rhbkwj7l9^c03^$cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
