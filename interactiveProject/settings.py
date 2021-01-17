87c673a5880522b8341f7b801ec43b72b74b0304
Ghios <56654758+Jbaker16163@users.noreply.github.com>
2021-01-16 21:05:50 -0500

    Possible Fix

--- interactiveProject/settings.py
+++ interactiveProject/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
     'interactiveApp',
     'accounts',
-    "crispy_forms",
 ]
 
 MIDDLEWARE = [
